88da828b605f8e8e170967c730a791704809544c
talessio-campanda <thiago.alessio@campanda.com>
2016-11-25 14:45:43 +0100

    Refactoring.

--- test/run.sh
+++ test/run.sh
@@ -3,11 +3,6 @@
 # setup
 cp my_file.original my_file
 
-# test cases
-sh ../parse-env.sh my_file
-status=$(diff my_file expected_file)
-
-# teardown
-rm my_file
-
-exit "$status"
+# test case
+sh ../parse-env my_file
+diff my_file expected_file
